f8e354e119bd6d0bbad2a892a15ea3bda292c816
Jake Kim <imjakekim@gmail.com>
2022-09-06 10:59:02 -0700

    modified relationships

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe '/projects/:id' do
+    before :each do
+        @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    end
+
+    it 'shows project name and material' do
+        visit "/projects/#{@news_chic.id}"
+        expect(page).to have_content(@news_chic.name)
+        expect(page).to have_content(@news_chic.material)
+        expect(page).to have_content(@recycled_material_challenge.theme)
+    end
+end
